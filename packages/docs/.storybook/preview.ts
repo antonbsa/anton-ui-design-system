84ef9d1babef709f0db5762de38a8124e0d3f659
Anton B <antonbsa.bck@gmail.com>
2025-10-10 14:01:03 -0300

    feat(dark-mode): add Storybook manager and preview configuration with dark theme

--- packages/docs/.storybook/preview.ts
+++ packages/docs/.storybook/preview.ts
@@ -2,10 +2,13 @@ import type { Preview } from '@storybook/react-vite'
 
 const preview: Preview = {
   parameters: {
+    themes: {
+      default: 'dark',
+    },
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
   },
